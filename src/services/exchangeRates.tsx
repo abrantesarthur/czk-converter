d6f76bde9addce6571ab59ed7052b25a3aad82bb
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-01-31 14:00:06 -0500

    implement parseExchangeRatesData

--- src/services/exchangeRates.tsx
+++ src/services/exchangeRates.tsx
@@ -1,27 +1,37 @@
 interface ExchangeRate {
-    country: string;
-    currency: string;
-    amount: number;
-    code: string;
-    rate: number;
+  country: string;
+  currency: string;
+  amount: number;
+  code: string;
+  rate: number;
 }
 
-const fecthExchangeRates = async () : Promise<ExchangeRate[]> => {
-    let exchangeRates : ExchangeRate[] = [];
-    try {
-        const response = await fetch("https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt");
-        const data = await response.text();
-        exchangeRates = parseExchangeRatesData(data);
-    } catch(_) {
-        return [];
-    }
-
-  return exchangeRates;
-}
-
-const parseExchangeRatesData = (data: string) : ExchangeRate[] => {
-    console.log(data);
+const fecthExchangeRates = async (): Promise<ExchangeRate[]> => {
+  try {
+    const response = await fetch(
+      "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
+    );
+    const data = await response.text();
+    return parseExchangeRatesData(data);
+  } catch (_) {
     return [];
-}
+  }
+};
 
-export {fecthExchangeRates};
+const parseExchangeRatesData = (data: string): ExchangeRate[] => {
+  return data
+    .split("\n")
+    .slice(2, -1)
+    .map((line) => {
+      let values = line.split("|");
+      return {
+        country: values[0],
+        currency: values[1],
+        amount: Number(values[2]),
+        code: values[3],
+        rate: Number(values[4]),
+      };
+    });
+};
+
+export { fecthExchangeRates };
