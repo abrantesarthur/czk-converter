c8bab7a83f48173f358395efbed0404eebf08db5
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-02-01 18:59:36 -0500

    add dropdown menu to form

--- src/components/ExchangeRateForm.tsx
+++ src/components/ExchangeRateForm.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 // TODO: allow only numbers to be inserted in the form
 export default function ExchangeRateForm() {
   const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("");
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const re = /^[0-9\b]+$/;
@@ -12,8 +13,13 @@ export default function ExchangeRateForm() {
     }
   };
 
+  const handleCurrencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrency(e.target.value);
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log("event submitted");
+    // TODO: what does this do?
+    e.preventDefault();
   };
 
   return (
@@ -21,22 +27,18 @@ export default function ExchangeRateForm() {
       <h1>form</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="amount">
-            Amount
-            <input
-              type="text"
-              id="amount"
-              value={amount}
-              onChange={handleAmountChange}
-            ></input>
-          </label>
-          <label htmlFor="amount">
-            Currency
-            <select>
-              <option value="usd">USD</option>
-              <option value="brl">BRL</option>
-            </select>
-          </label>
+          <label htmlFor="amount">Amount</label>
+          <input
+            type="text"
+            id="amount"
+            value={amount}
+            onChange={handleAmountChange}
+          ></input>
+          <label htmlFor="amount">Currency</label>
+          <select value={currency} onChange={handleCurrencyChange}>
+            <option value="usd">USD</option>
+            <option value="brl">BRL</option>
+          </select>
           <input type="submit" value="Convert"></input>
         </div>
       </form>
