e7c6cc0e1a80e7162ff6343b39bad0c89b3c79f9
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-02-02 12:43:36 -0500

    refactor ExchangeRate into a class

--- src/services/exchangeRates.tsx
+++ src/services/exchangeRates.tsx
@@ -1,45 +1,69 @@
-export interface ExchangeRates {
-  list: ExchangeRate[];
-}
-
-namespace ExchangeRates {
-  export const get = async (): Promise<ExchangeRates> => {
-    try {
-      const response = await fetch(
-        "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
-      );
-      const data = await response.text();
-      return parse(data);
-    } catch (_) {
-      return { list: [] };
-    }
-  };
-
-  const parse = (data: string): ExchangeRates => {
-    return {
-      list: data
-        .split("\n")
-        .slice(2, -1)
-        .map((line) => {
-          let values = line.split("|");
-          return {
-            country: values[0],
-            currencyName: values[1],
-            amount: Number(values[2]),
-            currencyCode: values[3],
-            rate: round(Number(values[4])),
-          };
-        }),
-    };
-  };
-}
-
-export interface ExchangeRate {
+export class ExchangeRate {
   country: string;
   currencyName: string;
   amount: number;
   currencyCode: string;
   rate: number;
+
+  constructor(
+    country: string,
+    currencyName: string,
+    amount: number,
+    currencyCode: string,
+    rate: number
+  ) {
+    this.country = country;
+    this.currencyName = currencyName;
+    this.amount = amount;
+    this.currencyCode = currencyCode;
+    this.rate = rate;
+  }
+
+  getRateFrom(currencyName: string) {
+    if ((currencyName = this.currencyName)) {
+      return this.rate;
+    }
+  }
+}
+
+export class ExchangeRates {
+  list: ExchangeRate[];
+
+  constructor(list: ExchangeRate[]) {
+    this.list = list;
+  }
+
+  get = async (): Promise<ExchangeRates> => {
+    try {
+      const response = await fetch(
+        "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
+      );
+      const data = await response.text();
+      return this.parse(data);
+    } catch (_) {
+      return new ExchangeRates([]);
+    }
+  };
+
+  getRatesFrom = (currencyCode: string) => {};
+
+  parse = (data: string): ExchangeRates => {
+    return new ExchangeRates(
+      data
+        .split("\n")
+        .slice(2, -1)
+        .map((line) => {
+          let values = line.split("|");
+          return new ExchangeRate(
+            values[0],
+            values[1],
+            Number(values[2]),
+            values[3],
+            round(Number(values[4]))
+          );
+        })
+    );
+  };
 }
 
 const round = (value: number, decimals = 2) => {
