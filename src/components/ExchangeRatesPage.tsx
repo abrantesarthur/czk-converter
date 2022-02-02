446d70cd22dc2e405227bd5347657a56019602a3
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-02-02 17:19:42 -0500

    refacto ExchangeRatesPage

--- src/components/ExchangeRatesPage.tsx
+++ src/components/ExchangeRatesPage.tsx
@@ -9,32 +9,20 @@ import { TableTitle } from "./styles/Table.styled.";
 export default function ExchangeRatesPage() {
   const { data, status } = useQuery("exchangeRates", ExchangeRates.get);
 
-  // TODO: fix loading screen
-  if (status === "loading") {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-
-  // TODO: Improve error screen
-  if (status === "error" || data === undefined) {
-    return (
-      <div>
-        <h1>Error...</h1>
-      </div>
-    );
-  }
-
   return (
     <Container>
-      <Column alignItems="flex-start">
-        <FormTitle>Convert Currencies to CZK</FormTitle>
-        <ExchangeRatesForm exchangeRates={data}></ExchangeRatesForm>
-        <TableTitle>CZK Conversion Table</TableTitle>
-        <ExchangeRatesTable exchangeRates={data}></ExchangeRatesTable>
-      </Column>
+      {status === "loading" ? (
+        <h1>Loading...</h1>
+      ) : status === "error" || data === undefined ? (
+        <h1>Something wrong happened. Try again later.</h1>
+      ) : (
+        <Column alignItems="flex-start">
+          <FormTitle>Convert Currencies to CZK</FormTitle>
+          <ExchangeRatesForm exchangeRates={data}></ExchangeRatesForm>
+          <TableTitle>CZK Conversion Table</TableTitle>
+          <ExchangeRatesTable exchangeRates={data}></ExchangeRatesTable>
+        </Column>
+      )}
     </Container>
   );
 }
