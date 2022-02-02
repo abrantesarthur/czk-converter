d19cbe868eb699b674a6bf43fbd931ba20d04bc3
Arthur Abrantes <abrantesarthur1997@gmail.com>
2022-02-01 19:54:21 -0500

    create ExchangeRates component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-import ExchangeRatesTable from "./components/ExchangeRatesTable";
 import { QueryClientProvider, QueryClient } from "react-query";
-import { Column, Container } from "./components/styles/Container.styled";
-import ExchangeRateForm from "./components/ExchangeRateForm";
-import { useQuery } from "react-query";
-import { fecthExchangeRates } from "./services/exchangeRates";
+import ExchangeRates from "./components/ExchangeRates";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const { data, status } = useQuery("exchangeRates", fecthExchangeRates);
-
-  // TODO: fix loading screen
-  if (status === "loading") {
-    return (
-      <div>
-        <h1>Loading...</h1>
-      </div>
-    );
-  }
-
-  // TODO: Improve error screen
-  if (status === "error" || data === undefined) {
-    return (
-      <div>
-        <h1>Error...</h1>
-      </div>
-    );
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
-      <Container>
-        <Column>
-          <ExchangeRateForm exchangeRates={data}></ExchangeRateForm>
-          <ExchangeRatesTable exchangeRates={data}></ExchangeRatesTable>
-        </Column>
-      </Container>
+      <ExchangeRates></ExchangeRates>
     </QueryClientProvider>
   );
 }
---
+++ src/components/ExchangeRates.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { fecthExchangeRates } from "../services/exchangeRates";
+import ExchangeRateForm from "./ExchangeRateForm";
+import ExchangeRatesTable from "./ExchangeRatesTable";
+import { Column, Container } from "./styles/Container.styled";
+
+export default function ExchangeRates() {
+  const { data, status } = useQuery("exchangeRates", fecthExchangeRates);
+
+  // TODO: fix loading screen
+  if (status === "loading") {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  // TODO: Improve error screen
+  if (status === "error" || data === undefined) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+  return (
+    <Container>
+      <Column>
+        <ExchangeRateForm exchangeRates={data}></ExchangeRateForm>
+        <ExchangeRatesTable exchangeRates={data}></ExchangeRatesTable>
+      </Column>
+    </Container>
+  );
+}
